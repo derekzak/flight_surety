9fbbedcb25141c486dbd8898f79868a73cbcb353
Derek Zak <zak@arizonabay.com>
2021-02-18 20:07:13 -0700

    Added remaiing test cases

--- test/flightSuretyTest.js
+++ test/flightSuretyTest.js
@@ -5,6 +5,8 @@ var BigNumber = require('bignumber.js');
 
 contract('Flight Surety Tests', async (accounts) => {
 
+    const TEST_ORACLES_COUNT = 20;
+    const STATUS_CODE_LATE_AIRLINE = 20;
     var config;
     before('setup contract', async () => {
         config = await Test.Config(accounts);
@@ -315,6 +317,54 @@ contract('Flight Surety Tests', async (accounts) => {
 
             // ASSERT
             assert.equal(result, true, "Passenger can buy insurance");
+        });
+
+        it('can register oracles', async () => {
+
+            // ARRANGE
+            let fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
+
+            // ACT
+            for(let a=1; a<TEST_ORACLES_COUNT; a++) {
+                await config.flightSuretyApp.registerOracle({ from: accounts[a+TEST_ORACLES_COUNT], value: fee });
+                let result = await config.flightSuretyApp.getMyIndexes.call({from: accounts[a+TEST_ORACLES_COUNT]});
+                //   console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`);
+            }
+        });
+
+        it('can request flight status', async () => {
+
+            // ACT
+            // Submit a request for oracles to get status information for a flight
+            await config.flightSuretyApp.fetchFlightStatus(config.flight1.airline, config.flight1.flight, config.flight1.timestamp);
+
+            // Since the Index assigned to each test account is opaque by design
+            // loop through all the accounts and for each account, all its Indexes (indices?)
+            // and submit a response. The contract will reject a submission if it was
+            // not requested so while sub-optimal, it's a good test of that feature
+            for(let a=1; a<TEST_ORACLES_COUNT; a++) {
+                // Get oracle information
+                let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({ from: accounts[a+TEST_ORACLES_COUNT]});
+                for(let idx=0;idx<3;idx++) {
+                    try {
+                        // Submit a response...it will only be accepted if there is an Index match
+                        await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.flight1.airline, config.flight1.flight, config.flight1.timestamp, STATUS_CODE_LATE_AIRLINE, { from: accounts[a+TEST_ORACLES_COUNT] });
+                    }
+                    catch(e) {
+                        // Enable this when debugging
+                        // console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
+                    }
+                }
+            }
+        });
+
+        it('(insurance) passenger is credited the correct amount for their insurance', async () => {
+
+            // ARRANGE
+            let amount = await config.flightSuretyData.getOutstandingPaymentAmount(config.passenger1);
+
+            // ASSERT
+            assert.equal(amount, web3.utils.toWei("0.5", "ether") * 1.5, "Insurance amount not calculated correctly");
         });
     });
 
